4c53cccbd13b32e5add51a3a1835fa35b4ef7291
MancillaO <edgarmncllav04@gmail.com>
2025-03-01 14:07:37 -0600

    Postgres and Mysql Connection

---
+++ presentacion/Cafeteria.java
@@ -0,0 +1,10 @@
+package presentacion;
+
+import negocio.Menu;
+
+public class Cafeteria {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        // menu.iniciar();
+    }
+}
