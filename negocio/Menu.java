1441afc4f23a8bcfee5243c4e4e05146709839ed
MancillaO <edgarmncllav04@gmail.com>
2025-03-01 23:16:34 -0600

    delete passwords

--- negocio/Menu.java
+++ negocio/Menu.java
@@ -1,5 +1,6 @@
 package negocio;
 
+import java.util.List;
 import java.util.Scanner;
 import datos.ConexionBD;
 
@@ -59,7 +60,7 @@ public class Menu {
                     case 4:
                         return null;
                     default:
-                        System.out.println("\nInvalid option. Please try again.");
+                        System.out.println("\nInvalid option. Please try again.\n");
                 }
             } catch (NumberFormatException e) {
                 System.out.println("\nInvalid input. Please enter a number.");
@@ -96,30 +97,31 @@ public class Menu {
                 opcion = Integer.parseInt(option);
                 switch (opcion) {
                     case 1:
-                        conexion.getAllPasswords();
+                        showPasswords();
                         break;
                     case 2:
-                        addNewPassword();
+                        addPassword();
                         break;
                     case 3:
                         if (!esMongoDB) {
                             searchByService();
                         } else {
-                            System.out.println("\nInvalid option. Please try again.");
+                            System.out.println("\nInvalid option. Please try again.\n");
                         }
                         break;
                     case 4:
                         if (!esMongoDB) {
                             updatePassword();
                         } else {
-                            System.out.println("\nInvalid option. Please try again.");
+                            System.out.println("\nInvalid option. Please try again.\n");
                         }
                         break;
                     case 5:
                         if (!esMongoDB) {
+                            allPasswords();
                             deletePassword();
                         } else {
-                            System.out.println("\nInvalid option. Please try again.");
+                            System.out.println("\nInvalid option. Please try again.\n");
                         }
                         break;
                     case 6:
@@ -135,11 +137,38 @@ public class Menu {
                         }
                         break;
                     default:
-                        System.out.println("\nInvalid option. Please try again.");
+                        System.out.println("\nInvalid option. Please try again.\n");
                 }
             } catch (NumberFormatException e) {
                 System.out.println("\nInvalid input. Please enter a number.");
             }
+        }
+    }
+
+    private void allPasswords() {
+        System.out.println("\n\n|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+        System.out.println("|                                                            |");
+        System.out.println("|                        ALL PASSWORDS                       |");
+        System.out.println("|                                                            |");
+        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+        System.out.println("|                                                            |");
+        conexion.getAllPasswords();
+        System.out.println("|                                                            |");
+        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+    }
+
+    private void showPasswords() {
+        allPasswords();
+        System.out.print(
+                "Type (D) to delete a password, (U) to update a password \nOr any other key to return to the main menu: ");
+        String option = scanner.nextLine();
+
+        if (option.equalsIgnoreCase("D")) {
+            deletePassword();
+        } else if (option.equalsIgnoreCase("U")) {
+            updatePassword();
+        } else {
+            return;
         }
     }
 
@@ -149,43 +178,104 @@ public class Menu {
         conexion.findPasswordsByService(servicio);
     }
 
-    private void addNewPassword() {
-        System.out.print("\nEnter service name: ");
-        String servicio = scanner.nextLine();
-        System.out.print("Enter username: ");
-        String username = scanner.nextLine();
-        System.out.print("Enter password: ");
-        String password = scanner.nextLine();
-        conexion.insertPassword(servicio, username, password);
+    private void addPassword() {
+        String servicio;
+        while (true) {
+            System.out.print("\nEnter service name: ");
+            servicio = scanner.nextLine().trim();
+            if (!servicio.isEmpty()) {
+                break;
+            }
+            System.out.println("\nError: Service name cannot be empty. Please try again.\n");
+        }
+        String username;
+        while (true) {
+            System.out.print("Enter username: ");
+            username = scanner.nextLine().trim();
+            if (!username.isEmpty()) {
+                break;
+            }
+            System.out.println("\nError: Username cannot be empty. Please try again.\n");
+        }
+        String password;
+        while (true) {
+            System.out.print("Enter password: ");
+            password = scanner.nextLine().trim();
+            if (!password.isEmpty()) {
+                break;
+            }
+            System.out.println("\nError: Password cannot be empty. Please try again.\n");
+        }
+        conexion.insertPassword(servicio.toUpperCase(), username, password);
     }
 
     private void updatePassword() {
-        System.out.print("\nEnter the ID of the password to update: ");
+        System.out.print("\nEnter the ID of the password to update \nOr press Enter to cancel: ");
+        String idInput = scanner.nextLine().trim();
+        if (idInput.isEmpty()) {
+            System.out.println("Update canceled.");
+            return;
+        }
+
+        int id;
         try {
-            int id = Integer.parseInt(scanner.nextLine());
-
-            System.out.print("Enter new service name: ");
-            String servicio = scanner.nextLine();
-
-            System.out.print("Enter new username: ");
-            String username = scanner.nextLine();
-
-            System.out.print("Enter new password: ");
-            String password = scanner.nextLine();
-
-            conexion.updatePassword(id, servicio, username, password);
+            id = Integer.parseInt(idInput);
         } catch (NumberFormatException e) {
             System.out.println("\nInvalid ID format. Please enter a number.");
+            return;
         }
+        String servicio;
+        while (true) {
+            System.out.print("Enter new service name: ");
+            servicio = scanner.nextLine().trim();
+            if (!servicio.isEmpty()) {
+                break;
+            }
+            System.out.println("\nError: Service name cannot be empty. Please try again.\n");
+        }
+        String username;
+        while (true) {
+            System.out.print("Enter new username: ");
+            username = scanner.nextLine().trim();
+            if (!username.isEmpty()) {
+                break;
+            }
+            System.out.println("\nError: Username cannot be empty. Please try again.\n");
+        }
+        String password;
+        while (true) {
+            System.out.print("Enter new password: ");
+            password = scanner.nextLine().trim();
+            if (!password.isEmpty()) {
+                break;
+            }
+            System.out.println("\nError: Password cannot be empty. Please try again.\n");
+        }
+        conexion.updatePassword(id, servicio, username, password);
     }
 
     private void deletePassword() {
-        System.out.print("\nEnter the ID of the password to delete: ");
+        System.out.print("\nEnter the ID of the password to delete\nOr press Enter to cancel: ");
+        String option = scanner.nextLine();
+
+        if (option.isEmpty()) {
+            System.out.println("\nOperation cancelled.");
+            return;
+        }
+
+        List<Integer> validIds = conexion.getValidIds();
+
         try {
-            int id = Integer.parseInt(scanner.nextLine());
-            conexion.deletePassword(id);
+            int id = Integer.parseInt(option);
+
+            if (validIds.contains(id)) {
+                conexion.deletePassword(id);
+            } else {
+                System.out.println("\nID not found in the database.");
+            }
         } catch (NumberFormatException e) {
             System.out.println("\nInvalid ID format. Please enter a number.");
         }
     }
+
 }
